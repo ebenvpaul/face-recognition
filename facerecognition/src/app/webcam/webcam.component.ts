17798064073a683df4efcd9f95f5e7fababa25a9
Eben Varghese Paul <eben92paul@gmail.com>
2024-10-20 01:10:12 +0530

    added PLATFORM_ID

--- facerecognition/src/app/webcam/webcam.component.ts
+++ facerecognition/src/app/webcam/webcam.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-webcam',
@@ -10,26 +11,31 @@ export class WebcamComponent implements OnInit {
   @ViewChild('videoElement') videoElement!: ElementRef;
   videoStream!: MediaStream;
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
     this.startWebcam();
   }
 
   startWebcam(): void {
-    // Check if mediaDevices and getUserMedia are supported
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream: MediaStream) => {
-          this.videoStream = stream;
-          this.videoElement.nativeElement.srcObject = this.videoStream;
-          this.videoElement.nativeElement.play();
-        })
-        .catch((err) => {
-          console.error("Error accessing webcam: ", err);
-        });
+    // Check if running in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      // Check if mediaDevices and getUserMedia are supported
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then((stream: MediaStream) => {
+            this.videoStream = stream;
+            this.videoElement.nativeElement.srcObject = this.videoStream;
+            this.videoElement.nativeElement.play();
+          })
+          .catch((err) => {
+            console.error("Error accessing webcam: ", err);
+          });
+      } else {
+        console.error("getUserMedia is not supported in this browser.");
+      }
     } else {
-      console.error("getUserMedia is not supported in this browser.");
+      console.warn("Webcam access is not available on the server side.");
     }
   }
 
